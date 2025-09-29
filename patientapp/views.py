cf10de9bf8534124d9542b3aa6e336d91a7b012a
julia topf <j.topf@htlkrems.at>
2025-09-29 15:38:49 +0200

    register added

--- patientapp/views.py
+++ patientapp/views.py
@@ -4,6 +4,8 @@ from patientapp.models import Patient
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -77,6 +79,19 @@ def perform_logout(request: HttpRequest):
     return redirect("/login/")
 
 def register(request: HttpRequest):
-    
-    pass
+    error_message = ""
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
 
+        if User.objects.filter(username=username).exists():
+            error_message = "Benutzername existiert bereits."
+        elif User.objects.filter(email=email).exists():
+            error_message = "E-Mail existiert bereits."
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            return redirect("/patients/")
+
+    return render(request, "register.html", {"error_message": error_message})
